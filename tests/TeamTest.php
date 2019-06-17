3baad04dd4e8340a77f18024993fd012cdd4dbd9
shahinyanm@gmail.com <shahinyanm@gmail.com>
2019-06-17 17:24:04 +0400

    Create TeamTest

---
+++ tests/TeamTest.php
@@ -0,0 +1,80 @@
+<?php
+
+use Laravel\Lumen\testing\DatabaseMigrations;
+use Laravel\Lumen\testing\DatabaseTransactions;
+
+class TeamTest extends testCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testShowAllTeams()
+    {
+        $this->get('api/teams', []);
+        $this->seeStatusCode(200);
+        $this->seeJson();
+//        $this->seeJsonStructure(
+//            ['*' => ['*' =>
+//                [
+//                    'id',
+//                    'name',
+//                    'email',
+//                    'created_at',
+//                    'updated_at',
+//                ]
+//            ]
+//            ]
+//        );
+        $this->assertTrue(true);
+    }
+
+    public function testTeamShow()
+    {
+        $this->get('api/teams/1', []);
+        $this->seeStatusCode(200);
+        $this->assertTrue(true);
+//        $this->seeJsonStructure(
+//            ['data' =>
+//                [
+//                    'id',
+//                    'name',
+//                    'email',
+//                    'created_at',
+//                    'updated_at',
+//                ]
+//            ]
+//        );
+    }
+
+    public function testTeamCreate()
+    {
+        $parameters = [
+            "title" => "Team",
+        ];
+        $this->post("api/teams", $parameters, []);
+        $this->seeStatusCode(201);
+        $this->seeInDatabase('teams', ['title' => 'Team']);
+    }
+
+
+    public function testTeamUpdate()
+    {
+        $parameters = [
+            "title" => "NewTeam",
+        ];
+        $this->put("api/teams/1", $parameters, []);
+        $this->seeStatusCode(200);
+        $this->seeInDatabase('teams', ['title' => 'NewTeam']);
+
+    }
+
+    public function testTeamDestroy()
+    {
+
+        $this->delete("api/teams/1", [], []);
+        $this->seeStatusCode(200);
+        $this->assertTrue(true);
+    }
+}
