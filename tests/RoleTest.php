a129fa4a3cc3aadda4094ec82b4034e0d64ba116
shahinyanm@gmail.com <shahinyanm@gmail.com>
2019-06-20 13:48:53 +0400

    bug fixed in RoleTest

--- tests/RoleTest.php
+++ tests/RoleTest.php
@@ -1,92 +1,107 @@
 <?php
 
+use App\Role;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Lumen\testing\DatabaseMigrations;
-use Laravel\Lumen\testing\DatabaseTransactions;
 
 class RoleTest extends TestCase
 {
-    use DatabaseMigrations;
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-protected static $user;
-    public function setUp(): void
-    {
-        parent::setUp();
+	use DatabaseMigrations;
+	/**
+	 * A basic test example.
+	 *
+	 * @return void
+	 */
+	protected static $user;
 
-        $this->runDatabaseMigrations();
-        if (is_null(self::$user)) {
-            self::$user = $user = factory(\App\User::class)->create([
-                'password' => \Illuminate\Support\Facades\Hash::make('secret'),
-            ]);
-        }
-    }
+	public function setUp(): void
+	{
+		parent::setUp();
 
-    public function testShowAllRoles()
-    {
-        $user = factory(\App\User::class)->create([
-            'password' => \Illuminate\Support\Facades\Hash::make('secret'),
-        ]);
-        $response = $this->actingAs(self::$user)->get('api/roles', []);
-        $this->seeStatusCode(200);
-        $this->seeJson();
-        $this->assertTrue(true);
-    }
+		$this->runDatabaseMigrations();
+		if (is_null(self::$user)) {
+			self::$user = $user = factory(User::class)->create([
+				'password' => Hash::make('secret'),
+			]);
+		}
+	}
 
-    public function testRoleShow()
-    {
-        $user = factory(\App\User::class)->create([
-            'password' => \Illuminate\Support\Facades\Hash::make('secret'),
-        ]);
-        $role= \App\Role::create(['title'=>'admin']);
-        $response = $this->actingAs(self::$user)->get('api/roles/'.$role->id);
-        $this->seeStatusCode(200);
-        $this->assertTrue(true);
+	public function testShowAllRoles()
+	{
+		$user = factory(User::class)->create([
+			'password' => Hash::make('secret'),
+		]);
+		$response = $this->actingAs(self::$user)->get('api/roles', []);
+		$this->seeStatusCode(200);
+		$response->assertResponseStatus(200);
+		$response->seeJsonStructure([
+			'*' => [
+				'id', 'title', 'created_at', 'updated_at',
+			]
+		]);
+		$response->assertResponseStatus(200);
+		$this->seeJson();
+	}
 
-    }
+	public function testRoleShow()
+	{
+		$user = factory(User::class)->create([
+			'password' => Hash::make('secret'),
+		]);
+		$role = Role::create(['title' => 'admin']);
+		$response = $this->actingAs(self::$user)->get('api/roles/' . $role->id);
+		$response->seeJsonStructure([
+			'id', 'title', 'created_at', 'updated_at',
+		]);
+		$response->assertResponseStatus(200);
+		$this->seeStatusCode(200);
 
-    public function testRoleCreate()
-    {
-        $user = factory(\App\User::class)->create([
-            'password'=>\Illuminate\Support\Facades\Hash::make('secret')
-        ]);
-        $parameters = [
-            "title" => "role",
-        ];
-        $response = $this->actingAs(self::$user)->post("api/roles", $parameters);
-        $this->seeStatusCode(201);
-        $this->seeInDatabase('roles', ['title' => 'role']);
-    }
+	}
+
+	public function testRoleCreate()
+	{
+		$user = factory(User::class)->create([
+			'password' => Hash::make('secret')
+		]);
+		$parameters = [
+			"title" => "role",
+		];
+		$response = $this->actingAs(self::$user)->post("api/roles", $parameters);
+		$this->seeStatusCode(201);
+		$response->assertResponseStatus(201);
+		$this->seeInDatabase('roles', ['title' => 'role']);
+	}
 
 
-    public function testRoleUpdate()
-    {
-        $user = factory(\App\User::class)->create([
-            'password'=>\Illuminate\Support\Facades\Hash::make('secret')
-        ]);
-        $parameters = [
-            "title" => "Newrole",
-        ];
-        $role = \App\Role::firstOrCreate([
-            'title'=>'owner'
-        ]);
-        $response = $this->actingAs(self::$user)->put("api/roles/".$role->id, $parameters);
-        $this->seeStatusCode(200);
-        $this->seeInDatabase('roles', ['title' => 'Newrole']);
+	public function testRoleUpdate()
+	{
+		$user = factory(User::class)->create([
+			'password' => Hash::make('secret')
+		]);
+		$parameters = [
+			"title" => "Newrole",
+		];
+		$role = Role::firstOrCreate([
+			'title' => 'owner'
+		]);
+		$response = $this->actingAs(self::$user)->put("api/roles/" . $role->id, $parameters);
+		$this->seeStatusCode(200);
+		$response->assertResponseOk(200);
+		$this->seeInDatabase('roles', ['title' => 'Newrole']);
 
-    }
+	}
 
-    public function testRoleDestroy()
-    {
-        $user = factory(\App\User::class)->create([
-            'password'=>\Illuminate\Support\Facades\Hash::make('secret')
-        ]);
-        $role = \App\Role::firstOrCreate([
-            'title'=>'owner'
-        ]);
-        $response = $this->actingAs(self::$user)->delete("api/roles/".$role->id, []);
-        $this->seeStatusCode(200);
-    }
+	public function testRoleDestroy()
+	{
+		$user = factory(User::class)->create([
+			'password' => Hash::make('secret')
+		]);
+		$role = Role::firstOrCreate([
+			'title' => 'owner'
+		]);
+		$response = $this->actingAs(self::$user)->delete("api/roles/" . $role->id, []);
+		$response->assertResponseStatus(200);
+		$this->seeStatusCode(200);
+	}
 }
